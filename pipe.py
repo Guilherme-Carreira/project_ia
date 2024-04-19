0186622f56f8d12f6698798cdc0466ffa15ce8af
Guilherme-Carreira <guilhermecarreira22@gmail.com>
2024-04-19 15:44:31 +0100

    lets go

---
+++ pipe.py
@@ -0,0 +1,9 @@
+class Board:
+    def adjacent_vertical_values(self, row: int, col, int):
+        pass
+    def adjacent_horizontal_values(self, row: int, col, int):
+        pass
+
+@staticmethod
+def parseinstance():
+    pass
