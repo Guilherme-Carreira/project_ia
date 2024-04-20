9bb8a7563f8a44fa8a3dce89efaf3d57ed8b726b
Marcio08 <marciofilipe2004@gmail.com>
2024-04-20 17:01:13 +0100

    spaces tab

--- pipe.py
+++ pipe.py
@@ -1,21 +1,20 @@
 from sys import stdin
 
 class Board:
-    def adjacent_vertical_values(self, row: int, col, int):
-        pass
-    def adjacent_horizontal_values(self, row: int, col, int):
-        pass
+  def adjacent_vertical_values(self, row: int, col, int):
+    pass
+  def adjacent_horizontal_values(self, row: int, col, int):
+    pass
 
 @staticmethod
 def parseinstance():
-    board = []
-    index = 0
-    while True:
-        line = stdin.readline()
-        if not line:
-            break
-        board[index] = line
-        index = index + 1
+  board = []
+  index = 0
+  while True:
+    line = stdin.readline()
+    if not line:
+      break
+    board[index] = line
+    index = index + 1
 
-
-    return Board(board)
+  return Board(board)
