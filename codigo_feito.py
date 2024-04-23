6a5014d8f0518b1284c105941ec0ba93dc0d1e8d
Marcio08 <marciofilipe2004@gmail.com>
2024-04-23 18:52:36 +0100

    update infos

---
+++ codigo_feito.py
@@ -0,0 +1,39 @@
+from sys import stdin
+
+class Board:
+
+  def __init__(self, grid: tuple): # definir o grid como um tuplo
+    self.grid = grid
+  
+  def adjacent_vertical_values(self, row: int, col: int): # acima e abaixo
+    if row == 0: # se for a primeira linha
+      return (None, self.grid[row + 1][col])
+    elif row == len(self.grid) - 1: # se for a ultima linha
+      return (self.grid[row - 1][col], None)
+    else: # se for qualquer outra linha no meio
+      return (self.grid[row - 1][col], self.grid[row + 1][col])
+
+  def adjacent_horizontal_values(self, row: int, col: int): # esquerda e direita
+    if row == 0:
+      return (None, self.grid[row][col + 1])
+    elif row == len(self.grid) - 1:
+      return (self.grid[row][col - 1], None)
+    else:
+      return (self.grid[row][col - 1], self.grid[row][col + 1])
+
+  @staticmethod
+  def parseinstance(): 
+    grid = []
+    for line in stdin: # le do stdin e o line e cada linha 
+      grid.append(tuple(line.strip().split(' '))) # strip() remove \n e \t do tuplo e split() divide 
+      if not line:
+        break
+    return Board(grid)
+
+board = Board.parseinstance()
+print(board.adjacent_vertical_values(0, 0))
+print(board.adjacent_horizontal_values(0, 0))
+
+print(board.adjacent_vertical_values(1, 1))
+print(board.adjacent_horizontal_values(1, 1))
+#print(board) #DEBUG 
--- pipe.py
+++ pipe.py
@@ -1,39 +1,113 @@
-from sys import stdin
+# pipe.py: Template para implementação do projeto de Inteligência Artificial 2023/2024.
+# Devem alterar as classes e funções neste ficheiro de acordo com as instruções do enunciado.
+# Além das funções e classes sugeridas, podem acrescentar outras que considerem pertinentes.
+
+# Grupo 05:
+# 106492 Marcio Simoes
+# 106690 Joao Guilherme Carreira
+
+import sys
+from search import (
+    Problem,
+    Node,
+    astar_search,
+    breadth_first_tree_search,
+    depth_first_tree_search,
+    greedy_search,
+    recursive_best_first_search,
+)
+
+
+class PipeManiaState:
+    state_id = 0
+
+    def __init__(self, board):
+        self.board = board
+        self.id = PipeManiaState.state_id
+        PipeManiaState.state_id += 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    # TODO: outros metodos da classe
+
 
 class Board:
+    """Representação interna de um tabuleiro de PipeMania."""
 
-  def __init__(self, grid: tuple): # definir o grid como um tuplo
-    self.grid = grid
-  
-  def adjacent_vertical_values(self, row: int, col: int): # acima e abaixo
-    if row == 0: # se for a primeira linha
-      return (None, self.grid[row + 1][col])
-    elif row == len(self.grid) - 1: # se for a ultima linha
-      return (self.grid[row - 1][col], None)
-    else: # se for qualquer outra linha no meio
-      return (self.grid[row - 1][col], self.grid[row + 1][col])
+    def get_value(self, row: int, col: int) -> str:
+        """Devolve o valor na respetiva posição do tabuleiro."""
+        # TODO
+        pass
 
-  def adjacent_horizontal_values(self, row: int, col: int): # esquerda e direita
-    if row == 0:
-      return (None, self.grid[row][col + 1])
-    elif row == len(self.grid) - 1:
-      return (self.grid[row][col - 1], None)
-    else:
-      return (self.grid[row][col - 1], self.grid[row][col + 1])
+    def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
+        """Devolve os valores imediatamente acima e abaixo,
+        respectivamente."""
+        # TODO
+        pass
 
-  @staticmethod
-  def parseinstance(): 
-    grid = []
-    for line in stdin: # le do stdin e o line e cada linha 
-      grid.append(tuple(line.strip().split(' '))) # strip() remove \n e \t do tuplo e split() divide 
-      if not line:
-        break
-    return Board(grid)
+    def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
+        """Devolve os valores imediatamente à esquerda e à direita,
+        respectivamente."""
+        # TODO
+        pass
 
-board = Board.parseinstance()
-print(board.adjacent_vertical_values(0, 0))
-print(board.adjacent_horizontal_values(0, 0))
+    @staticmethod
+    def parse_instance():
+        """Lê o test do standard input (stdin) que é passado como argumento
+        e retorna uma instância da classe Board.
 
-print(board.adjacent_vertical_values(1, 1))
-print(board.adjacent_horizontal_values(1, 1))
-#print(board) #DEBUG '''
+        Por exemplo:
+            $ python3 pipe.py < test-01.txt
+
+            > from sys import stdin
+            > line = stdin.readline().split()
+        """
+        # TODO
+        pass
+
+    # TODO: outros metodos da classe
+
+
+class PipeMania(Problem):
+    def __init__(self, board: Board):
+        """O construtor especifica o estado inicial."""
+        # TODO
+        pass
+
+    def actions(self, state: PipeManiaState):
+        """Retorna uma lista de ações que podem ser executadas a
+        partir do estado passado como argumento."""
+        # TODO
+        pass
+
+    def result(self, state: PipeManiaState, action):
+        """Retorna o estado resultante de executar a 'action' sobre
+        'state' passado como argumento. A ação a executar deve ser uma
+        das presentes na lista obtida pela execução de
+        self.actions(state)."""
+        # TODO
+        pass
+
+    def goal_test(self, state: PipeManiaState):
+        """Retorna True se e só se o estado passado como argumento é
+        um estado objetivo. Deve verificar se todas as posições do tabuleiro
+        estão preenchidas de acordo com as regras do problema."""
+        # TODO
+        pass
+
+    def h(self, node: Node):
+        """Função heuristica utilizada para a procura A*."""
+        # TODO
+        pass
+
+    # TODO: outros metodos da classe
+
+
+if __name__ == "__main__":
+    # TODO:
+    # Ler o ficheiro do standard input,
+    # Usar uma técnica de procura para resolver a instância,
+    # Retirar a solução a partir do nó resultante,
+    # Imprimir para o standard output no formato indicado.
+    pass
